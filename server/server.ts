a1f7824dda5061a10579477be004da30b68517ab
Chandeshwar Reddy <challachandeshwarreddy@gmail.com>
2026-07-27 19:32:52 +0530

    creted api for social account creation

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRouter from "./routes/authRouter.js";
+import socialAuthRouter from "./routes/socialAuthRouters.js";
 
 const app = express();
 
@@ -17,6 +19,9 @@ app.get('/', (_req: Request, res: Response) => {
     res.send('Server is Live!');
 });
 
+
+app.use("/api/auth",authRouter);
+app.use("/api/Oauth",socialAuthRouter)
 //Global Error handler
 
 app.use((err:any,_req:Request,res:Response,_next:NextFunction)=>{
