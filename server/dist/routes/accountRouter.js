202449ca78fa224aa7d6ad8fca28305cdcef03d6
Chandeshwar Reddy <challachandeshwarreddy@gmail.com>
2026-08-06 21:09:06 +0530

    backend developement completed

--- server/dist/routes/accountRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { protect } from "../middlewares/authMiddleware.js";
-import { getAccounts, addAccount, disconnectAccount } from "../controllers/accountController.js";
-const accountRouter = express.Router();
-accountRouter.get("/", protect, getAccounts);
-accountRouter.post("/", protect, addAccount);
-accountRouter.delete("/:id", protect, disconnectAccount);
-export default accountRouter;
